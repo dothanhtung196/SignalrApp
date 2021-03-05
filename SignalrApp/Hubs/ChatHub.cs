91a4a1c029c06f07b9d64e66f18a9d39dd5a2ec5
Thanh Tung Do <dothanhtung196@gmail.com>
2021-03-05 14:03:03 +0700

    update source

--- SignalrApp/Hubs/ChatHub.cs
+++ SignalrApp/Hubs/ChatHub.cs
@@ -20,23 +20,20 @@ namespace SignalrApp.Hubs
         public async Task AddToGroup(string groupName)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-            await Clients.Group(groupName).SendAsync("ReceiveMessageFromGroup", $"{Context.ConnectionId} has joined the group {groupName}.");
+            await Clients.Group(groupName).SendAsync("ReceiveMessageNotificationGroup", $"{Context.ConnectionId} has joined the group {groupName}.");
         }
-
         public async Task RemoveFromGroup(string groupName)
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
-            await Clients.Group(groupName).SendAsync("ReceiveMessageFromGroup", $"{Context.ConnectionId} has left the group {groupName}.");
+            await Clients.Group(groupName).SendAsync("ReceiveMessageNotificationGroup", $"{Context.ConnectionId} has left the group {groupName}.");
         }
-
         public Task SendMessageToGroup(string groupName, string user, string message)
         {
             return Clients.Group(groupName).SendAsync("ReceiveMessageFromGroup", user, message);
         }
-
         public Task SendMessageToUser(string connectionId, string message)
         {
-            return Clients.Client(connectionId).SendAsync("ReceiveMessageFromUser", message);
+            return Clients.Client(connectionId).SendAsync("ReceiveMessageFromUser", connectionId, message);
         }
     }
 }
--- SignalrApp/wwwroot/js/chat.js
+++ SignalrApp/wwwroot/js/chat.js
@@ -4,33 +4,71 @@ connection.on("ReceiveMessageStart", function (user, message) {
     console.log(`Hi ${user}, you said: ${message}`)
 });
 
-connection.on("ReceiveMessageFromGroup", function(message){
+connection.on("ReceiveMessageNotificationGroup", function(message){
     console.log(message);
+});
+
+connection.on("ReceiveMessageFromGroup", function(user, message){
+    console.log(`${user}: ${message}`);
 });
 
 connection.on("ReceiveMessage", function(user, message){
     console.log(`${user}: ${message}`)
 })
 
+connection.on("ReceiveMessageFromUser", function(connection_id, message){
+    console.log(`${connection_id}: ${message}`);
+});
+
 connection.start().then(function () {
-    // connection.invoke("GetConnectionId").then((connectionId)=>console.log("ConnectionId: " + connectionId));
-    console.log("ConnectionId: " + this.connection.connection.connectionId);
+    connection.invoke("GetConnectionId").then((connectionId)=>$("#my-connection-id").val(connectionId));
+    
+    // console.log("ConnectionId: " + this.connection.connection.connectionId);
     connection.invoke("SendMessageStart", 'Will', 'Hello World').catch(function (err) {
         return console.error(err.toString());
-    });
-    connection.invoke("AddToGroup", "Group1").catch(function (error) { 
-        return console.error(error.toString())
     });
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
-
-// Form
+// Jquery Form
 $("#chat-button").click(function(){
     var username = $("#username").val();
     var content = $("#content").val();
     connection.invoke("SendMessageToAll", username, content).catch(function(error){
         return console.error(error.toString());
     });
-})
+});
+
+$("#add-group-button").click(function(){
+    var group_name = $("#name-group").val();
+    connection.invoke("AddToGroup", group_name).catch(function(error){
+        return console.error(error.toString());
+    });
+});
+
+$("#remove-group-button").click(function(){
+    var group_name = $("#name-group").val();
+    connection.invoke("RemoveFromGroup", group_name).catch(function(error){
+        return console.error(error.toString());
+    });
+});
+
+$("#chat-group-button").click(function(){
+    var group_name = $("#name-group").val();
+    var username = $("#username-group").val();
+    var content = $("#content-group").val();
+    connection.invoke("SendMessageToGroup", group_name, username, content).catch(function(error){
+        return console.error(error.toString());
+    });
+});
+
+$("#chat-single-button").click(function(){
+    var connection_id = $("#connection-id").val();
+    var content = $("#content-single").val();
+    connection.invoke("SendMessageToUser", connection_id, content).catch(function(error){
+        return console.error(error.toString());
+    });
+});
+
+
